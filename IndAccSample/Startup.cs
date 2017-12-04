bf058bb1785a99016e422bfa08d8a7e57c5411b3
Jake Bladt <jake@jakebladt.com>
2017-12-04 15:30:16 -0500

    initial commit

---
+++ IndAccSample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IndAccSample.Startup))]
+namespace IndAccSample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
